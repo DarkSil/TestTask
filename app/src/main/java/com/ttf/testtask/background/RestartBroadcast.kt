ef36dd31dac13bdc4ab5d50e6d115066fba01419
DarkSil <marenkov.ilio@gmail.com>
2023-05-12 17:23:02 +0200

    DB Save

--- app/src/main/java/com/ttf/testtask/background/RestartBroadcast.kt
+++ app/src/main/java/com/ttf/testtask/background/RestartBroadcast.kt
@@ -4,11 +4,19 @@ import android.content.BroadcastReceiver
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
+import com.ttf.testtask.util.DBHelper
 
 class RestartBroadcast : BroadcastReceiver() {
     override fun onReceive(p0: Context?, p1: Intent?) {
         if (p1?.action == Intent.ACTION_BOOT_COMPLETED) {
+            p0?.let {
+                val db = DBHelper(it).writableDatabase
 
+                val contentValues = ContentValues()
+                contentValues.put("time", System.currentTimeMillis().toString())
+
+                db.insert(DBHelper.TABLE_NAME, null, contentValues)
+            }
         }
     }
 }
